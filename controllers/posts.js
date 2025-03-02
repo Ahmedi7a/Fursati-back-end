29ed9b0f089c287e474e1bf87e409035536c7c79
lateefa <aldoseri369@gmail.com>
2025-03-02 15:03:09 +0300

    test

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,14 +13,62 @@ const router = express.Router();
 router.use(verifyToken);
 
 
+router.get('/', async (req, res) => {
+    try {
+      const hoots = await Post.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+
+  router.get('/:postId', async (req, res) => {
+    try {
+      const hoot = await Post.findById(req.params.hootId).populate('author');
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 
 
+  router.post('/', async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+      const post = await Post.create(req.body);
+      hoot._doc.author = req.user;
+      res.status(201).json(hoot);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  });
+
+  router.post('/:postId/comments', async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+      const hoot = await Hoot.findById(req.params.hootId);
+      hoot.comments.push(req.body);
+      await hoot.save();
 
 
 
-
-
+  
+      // Find the newly created comment:
+ const newComment = hoot.comments[hoot.comments.length - 1];
+  
+      newComment._doc.author = req.user;
+  
+      // Respond with the newComment:
+      res.status(201).json(newComment);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 
 module.exports = router;
